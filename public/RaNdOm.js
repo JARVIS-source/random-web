d4476cacb9e9a663b80417b0868d3da96a733872
JARVIS-source <jarvis@j-a-r-v-i-s.localdomain>
2020-10-25 00:57:51 +0200

    Next

--- public/RaNdOm.js
+++ public/RaNdOm.js
@@ -1,5 +1,6 @@
 var options = [];
 var To_Do = false;
+var a = true;
 
 function add_option() {
     rotateAnimation("logo");
@@ -7,9 +8,6 @@ function add_option() {
     if (option != "" && option != " " && option != "  " && option != "   " && option != "    " && option != "     ") {
         options.push(option);
         document.getElementById("input_box").value = "";
-        // var para = document.createElement("P");
-        // para.innerHTML = option;
-        // document.getElementById("myDIV").appendChild(para);
     }
 }
 
@@ -61,16 +59,16 @@ function process() {
 
 function choose() {
     rotateAnimation("logo");
-    var mode = document.getElementById("mode").value;
+    var mode = document.getElementById("display").value;
     if (mode != "To-Do") {
-        document.getElementById("mode").value = "To-Do";
-        document.getElementById("mode").innerHTML = "To-Do";
+        document.getElementById("display").value = "To-Do";
+        document.getElementById("display").innerHTML = "To-Do";
         document.getElementById("display").style.background = '#9f05c575';
         document.getElementById("display").style.color = '#ffffff';
         return To_Do = true;
     } else if (mode == "To-Do") {
-        document.getElementById("mode").value = "Random_Decission";
-        document.getElementById("mode").innerHTML = "Random_Decission";
+        document.getElementById("display").value = "Random_Decission";
+        document.getElementById("display").innerHTML = "Random_Decission";
         document.getElementById("display").style.background = '';
         document.getElementById("display").style.color = '#812697e7';
         return To_Do = false;
@@ -95,4 +93,34 @@ function rotateAnimation() {
 function del() {
     rotateAnimation("logo");
     options = [];
+}
+
+function info() {
+    rotateAnimation("logo");
+    alert("This is the Infosite\n\nThe intesion of this site is to automate making trivial decission\n\nHow to use:\nPut in your options (enter or ⚡) and click the R@ŊðØm button\nto get an answer or switch the toogle button so you will see a To-Do order\nClick the 📑 emoji to see again what you have entered\nClick the 🤯 to delete the given input\n\nFor color changing use the toogle button in the info bar\n\n\ncontact: j_a_r_v_i_s@mail.de")
+}
+
+function black_out() {
+    rotateAnimation("logo");
+    if (a == true) {
+        for (i = 0; i < 2; i++) {
+            var color = document.body.style.backgroundColor;
+            if (color == "rgba(17, 31, 41, 0.965)") {
+                document.body.style.backgroundColor = "#ffffff";
+                document.getElementById("logo").src = "%PUBLIC_URL%/button-rand.png";
+            } else {
+                document.body.style.backgroundColor = "rgba(17, 31, 41, 0.965)";
+                document.getElementById("logo").src = "%PUBLIC_URL%/silverbutton-rand.png";
+            };
+        } return a = false;
+    } else {
+        var color = document.body.style.backgroundColor;
+        if (color == "rgba(17, 31, 41, 0.965)") {
+            document.body.style.backgroundColor = "#ffffff";
+            document.getElementById("logo").src = "%PUBLIC_URL%/button-rand.png";
+        } else {
+            document.body.style.backgroundColor = "rgba(17, 31, 41, 0.965)";
+            document.getElementById("logo").src = "%PUBLIC_URL%/silverbutton-rand.png";
+        };
+    }
 }
